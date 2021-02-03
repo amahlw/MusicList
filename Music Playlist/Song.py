048b6100e3a34767a9215834dc562f00e0526854
Kash <amahlw@gmail.com>
2021-02-03 01:09:02 -0500

    Update Song.py

--- Music Playlist/Song.py
+++ Music Playlist/Song.py
@@ -17,16 +17,17 @@ class Song:
     # TODO: Create a getter method for the next_song attribute, called get_next_song
 
     def get_next_song(self):
-        pass
+        return self.__next_song
 
     # TODO: Create a setter method for the next_song attribute, called set_next_song
 
     def set_next_song(self, next_title):
-        pass
+        self.next_title = next_title
 
     # TODO: Using the __str___ dunder method, return a string of the song title.
 
     def __str__(self):
+        return str
         pass
 
     # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
